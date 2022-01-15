ee8d23f1b4d9fb86ddb87371bc2eafa3158f9521
maitrey2711 <96682223+maitrey2711@users.noreply.github.com>
2022-01-15 22:35:45 +0530

    Add files via upload

---
+++ diction.py
@@ -0,0 +1,38 @@
+#Maitrey Patel
+#20ce090
+
+
+#DICTIONARIES
+#a. Write a Python script to check whether a given key already exists in a dictionary.
+d = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+x=5
+if x in d:
+    print('Key is present in the dictionary')
+else:
+    print('Key is not present in the dictionary')
+
+#b. Write a Python script to merge two Python dictionaries.
+d1 = {0:100, 1:200}
+d2 = {2:300, 3:400}
+d1.update(d2)
+print(d1)
+
+#c. Write a Python program to sum all the items in a dictionary.
+dic = {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
+sum =0
+for i in dic.values():
+    sum+=i
+print("Sum: ",sum)
+
+#d. Write a Python script to add a key to a dictionary.
+sam_dic = {'a':10,'b':20, 'c':30}
+sam_dic.update({'d':40})
+print(sam_dic)
+
+#e. Write a Python script to concatenate following dictionaries to create a new one.
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50,6:60}
+dic1.update(dic2)
+dic1.update(dic3)
+print(dic1)
---
+++ sets.py
@@ -0,0 +1,33 @@
+#Maitrey Patel
+#20ce090
+
+
+#SETS
+#a. Write a Python program to add member(s) in a set and clear a set
+set1 = set()
+set1.add(0)
+set1.add(1)
+print(set1)
+set1.clear()
+print(set1)
+
+#b. Write a Python program to remove an item from a set if it is present in the set.
+set2 = {0,1,2,3,4,5}
+x = 3
+if x in set2:
+    set2.remove(x)
+print(set2)
+
+#c. Write a Python program to create an intersection, Union, difference of sets.
+s1 = {1,2,3,4,5}
+s2 = {2,4,6,8,0}
+inters = s1 & s2
+union = s1|s2
+dif = s1-s2
+print("Intersection :",inters)
+print("Union: ",union)
+print("Difference: ",dif)
+
+#d. Write a Python program to find maximum and the minimum value in a set.
+print("Maximum: :",max(set2))
+print("Minimum: ",min(set2))
